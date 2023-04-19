031991919b94aa61e1e62ae4f6f8b38259c0f2af
haitbaeev <haitbaevh@mail.ru>
2023-04-19 08:42:25 +0600

    first commit

---
+++ lesson4.py
@@ -0,0 +1,89 @@
+#if
+# num1 = 6
+# num2 = 5
+# if num1 > num2 and num2 == 9:
+#     print(num2 + num1)
+# elif num2 > num1:
+#     print(num1 - num2)
+# else:
+#     print("the end")
+
+# number_1 = int(input("Введите 1ое число: "))
+# number_2 = int(input("Введите 2ое число: "))
+# oper = input("Введите оператор: ")
+# if oper == "+":
+#     print(number_1 + number_2)
+# elif oper == "_":
+#     print(number_1 - number_2)
+# elif oper == "*":
+#     print(number_1 * number_2)
+# elif oper == "/":
+#     print(number_1 / number_2)
+# else:
+#     print("operator not found!")
+
+# gradus = int(input("Введите температуру: "))
+# gradusf = gradus * 9/5 + 32
+# c = -273.15
+# if gradusf < c:
+#     print("Температура недействительна!")
+# elif gradusf == c:
+#     print("Температура равна 0")
+# elif c < gradusf < 0:
+#     print("Температура ниже точки замерзания")
+# elif gradusf == 0:
+#     print("Температура в зоне замерзания")
+# elif gradusf == 100:
+#     print("Температура в точке кипения")
+# elif gradusf > 100:
+#     print("Температура выше точки кипения")
+
+# def is_leap_year(year):
+#     if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+#         return "YES"
+#     else:
+#         return "NO"
+# year = int(input("Введите год: "))
+# print(is_leap_year(year))
+
+num_items = int(input("Введите количество предмет
+
+#lists
+# num = 1
+# num2 = 3
+# my_list = [1, 2]
+# my_list[2] = "hasan"
+# print(my_list[2])
+
+# methods
+
+# for i in my_list:
+    # print(i)
+    # print(i)
+    # if type(i) == str:
+    #     print(i.upper())
+
+# my_list = [1, 3, 4, 5, 6,]
+# my_list.append(4)
+# my_list.append(3)
+# print(my_list.append((3))
+# print(my_list + [2, 3])
+# my_list.extend(["fafa", "fafa"])
+# print(my_list)
+# print(dir(my_list))
+# my_list.insert(3, "Hasan")
+# print(my_list)
+# my_list.sort()
+# my_list = [1, 3, 4, 5, 6,]
+# my_list.reverse()
+# print(my_list)
+# str_list = ["A", "b","c","C"]
+# str_list.sort(key=str.upper, reverse=True)
+# print(str_list)
+# phones = ["99655443322", "996777665544"]
+# phones.remove("996777665544")
+# print(phones.count("996777665544"))
+# print(phones.index("996777665544", 1))
+# black_phone = phones.pop(1)
+# print(phones)
+# print(black_phone)
---
+++ lesson5.py
@@ -0,0 +1,32 @@
+#dict
+# словари
+# my_dict = {}
+# my_dict = dict()
+# my_dict = {"name": "Hasan","age": 18,"phone_number": "+996708888072"}
+# print(my_dict["name"])
+# print(my_dict["phone_number"])
+# for i in my_dict:
+#     print(i)
+# print(my_dict.items())
+
+# account = {"Username": [],"email": [],"password":[]}
+# username = input("Введите ваше имя: ")
+# email = input("Введите ваш email: ")
+# password = input("Введите ваш пароль: ")
+# def validate_password(password):
+#     if len(password) < 8:
+#         return False
+#     if not any(char.isupper() for char in password):
+#         return False
+#     return True
+# if validate_password(password):
+#     print("Password is valid")
+#     account["password"] = [password]
+# else:
+#     print("Password is not valid")
+# account["Username"] = [username]
+# if email.endswith("@mail.ru")\
+#     or email.endswith("@gmail.com")\
+#     or email.endswith("@outbook.com"):
+#     account["email"] = [email]
+# print(account)
